54c89870e497acbb65fd8be420b57fe8718d8702
Jeanine Lilleng <jeanine@comoyo.com>
2013-01-21 13:55:05 +0100

    Comoyo version of HIVE_TEST

--- src/main/java/com/jointhegrid/hive_test/HiveTestBase.java
+++ src/main/java/com/jointhegrid/hive_test/HiveTestBase.java
@@ -33,34 +33,25 @@ public abstract class HiveTestBase extends HadoopTestCase {
   public HiveTestBase() throws IOException {
     super(HadoopTestCase.LOCAL_MR, HadoopTestCase.LOCAL_FS, 1, 1);
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    Map<String, String> env = new HashMap<String, String>();
-    env.putAll(System.getenv());
-    if (System.getenv("HADOOP_HOME") == null) {
-      String shome = System.getProperty("user.home");
-      if (shome != null) {
-        File home = new File(shome);
-        File hadoopR = new File(home, "hadoop");
-        File hadoopHome = new File(hadoopR, "hadoop-0.20.2_local");
-        if (hadoopHome.exists()) {
-          env.put("HADOOP_HOME", hadoopHome.getAbsolutePath());
+      try {
+          Thread.sleep(1000);
+      } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+      }
+      Map<String, String> env = new HashMap<String, String>();
+      env.putAll(System.getenv());
+    if (System.getenv("HADOOP_HOME ") == null) {
+        File hadoopInstall = findHadoopInstall();
+        if (hadoopInstall != null ){
+          env.put("HADOOP_HOME", hadoopInstall.getAbsolutePath());
           EnvironmentHack.setEnv(env);
-        }
-	File target = new File("target/hadoop-0.20.2");
-        if ( target.exists() ){
-          env.put("HADOOP_HOME", target.getAbsolutePath());
-          EnvironmentHack.setEnv(env);
-        }
+        } else {
+            throw new IOException("No HADOOP_HOME or local hadoop install in target found");
       }
     }
   }
-
-  protected Path getDir(Path dir) {
+    protected Path getDir(Path dir) {
     if (isLocalFS()) {
       String localPathRoot = System.getProperty("test.build.data", "/tmp").replace(' ', '+');
       dir = new Path(localPathRoot, dir);
@@ -79,9 +70,23 @@ public abstract class HiveTestBase extends HadoopTestCase {
     FileSystem fs = FileSystem.get(conf);
     fs.delete(rootDir, true);
     Path metastorePath = new Path("/tmp/metastore_db");
-    fs.delete(metastorePath, true);
-    Path warehouse = new Path("/tmp/warehouse");
-    fs.delete(warehouse, true);
-    fs.mkdirs(warehouse);
+      fs.delete(metastorePath, true);
+      Path warehouse = new Path("/tmp/warehouse");
+      fs.delete(warehouse, true);
+      fs.mkdirs(warehouse);
   }
+  private File findHadoopInstall() {
+      File target = new File("target");
+      File[] content = target.listFiles();
+      boolean found = false;
+      File hadoopPath = null;
+      for (int i = 0; i < content.length; i++) {
+          File currentFile = content[i];
+          if (currentFile.isDirectory() && currentFile.getName().startsWith("hadoop-")) {
+              return currentFile;
+          }
+      }
+      return null;
+  }
+
 }
